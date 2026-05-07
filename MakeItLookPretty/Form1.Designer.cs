5d599da3ed6d26f2d4dd4b9b787dac27d4be1732
craftyglitch1 <mrjames.bert@gmail.com>
2026-05-07 14:47:44 +1000

    Add project files.

---
+++ MakeItLookPretty/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MakeItLookPretty
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comBoxColor = new ComboBox();
+            txtColorCode = new TextBox();
+            label1 = new Label();
+            btnConfirm = new Button();
+            SuspendLayout();
+            // 
+            // comBoxColor
+            // 
+            comBoxColor.FormattingEnabled = true;
+            comBoxColor.Items.AddRange(new object[] { "Left half of window coloured", "Right half of window coloured", "Whole window coloured", "Normal" });
+            comBoxColor.Location = new Point(292, 337);
+            comBoxColor.Name = "comBoxColor";
+            comBoxColor.Size = new Size(184, 28);
+            comBoxColor.TabIndex = 0;
+            comBoxColor.SelectionChangeCommitted += comBoxColor_SelectionChangeCommitted;
+            // 
+            // txtColorCode
+            // 
+            txtColorCode.Location = new Point(292, 371);
+            txtColorCode.Name = "txtColorCode";
+            txtColorCode.Size = new Size(184, 27);
+            txtColorCode.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(198, 371);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Hex Code";
+            // 
+            // btnConfirm
+            // 
+            btnConfirm.Location = new Point(292, 404);
+            btnConfirm.Name = "btnConfirm";
+            btnConfirm.Size = new Size(184, 29);
+            btnConfirm.TabIndex = 3;
+            btnConfirm.Text = "Confirm";
+            btnConfirm.UseVisualStyleBackColor = true;
+            btnConfirm.Click += btnConfirm_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnConfirm);
+            Controls.Add(label1);
+            Controls.Add(txtColorCode);
+            Controls.Add(comBoxColor);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comBoxColor;
+        private TextBox txtColorCode;
+        private Label label1;
+        private Button btnConfirm;
+    }
+}
---
+++ MakeItLookPretty/Form1.cs
@@ -0,0 +1,122 @@
+namespace MakeItLookPretty
+{
+    public partial class Form1 : Form
+    {
+        const int WM_PAINT = 0x000F;
+        bool? leftHalf;
+
+        Color windowColor;
+        Brush windowBrush;
+        Bitmap screen;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            screen = new Bitmap(this.Width, this.Height);
+            this.DrawToBitmap(screen, new Rectangle(0, 0, this.Width, this.Height));
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            comBoxColor.Enabled = false;
+            double bright = getPixelBrightness(screen.GetPixel(0, 0));
+            MessageBox.Show(bright.ToString());
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            switch(m.Msg)
+            {
+                case WM_PAINT:
+                    Graphics g = Graphics.FromHwnd(this.Handle);
+                    if (leftHalf == true)
+                    {
+                        Rectangle rect = new Rectangle(0, 0, this.Width / 2, this.Height);
+                        g.FillRectangle(windowBrush, rect);
+                    }
+                    else if (leftHalf == false)
+                    {
+                        Rectangle rect = new Rectangle(409, 0, this.Width / 2, this.Height);
+                        g.FillRectangle(windowBrush, rect);
+                    }
+                    g.Dispose();
+                    break;
+            }
+        }
+
+        private void comBoxColor_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            switch (comBoxColor.SelectedIndex)
+            {
+                case 0:
+                    this.BackColor = SystemColors.Control;
+                    foreach(Control control in this.Controls)
+                       control.BackColor = ColorTranslator.FromHtml(txtColorCode.Text);
+                    leftHalf = true;
+                    this.Refresh();
+                    break;
+                case 1:
+                    foreach (Control control in this.Controls)
+                        control.BackColor = SystemColors.Control;
+                    this.BackColor = SystemColors.Control;
+                    leftHalf = false;
+                    this.Refresh();
+                    break;
+                case 2:
+                    foreach (Control control in this.Controls)
+                        control.BackColor = ColorTranslator.FromHtml(txtColorCode.Text);
+                    leftHalf = null;
+                    this.Refresh();
+                    this.BackColor = ColorTranslator.FromHtml(txtColorCode.Text);
+                    break;
+                case 3:
+                    foreach (Control control in this.Controls)
+                        control.BackColor = SystemColors.Control;
+                    leftHalf = null;
+                    this.Refresh();
+                    this.BackColor = SystemColors.Control;
+                    break;
+            }
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            if(txtColorCode.Text != string.Empty)
+            {
+                bool valid = txtColorCode.Text.StartsWith('#');
+                bool validLength = txtColorCode.Text.Length == 7;
+                string validChars = "0123456789ABCDEF";
+
+                bool hasValidChars = true;
+                foreach(char letter in txtColorCode.Text.Substring(1).ToUpper())
+                {
+                    if(!validChars.Contains(letter))
+                    {
+                        hasValidChars = false;
+                        break;
+                    }
+                }
+                if(valid && validLength && hasValidChars)
+                {
+                    windowColor = ColorTranslator.FromHtml(txtColorCode.Text);
+                    windowBrush = new SolidBrush(windowColor);
+                    comBoxColor.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("invalud");
+                }
+            }
+        }
+
+        private double getPixelBrightness(Color color)
+        {
+            // https://stackoverflow.com/questions/26233781/detect-the-brightness-of-a-pixel-or-the-area-surrounding-it
+            return (0.2126 * color.R + 0.7152 * color.G + 0.0722 * color.B);
+        }
+    }
+}
